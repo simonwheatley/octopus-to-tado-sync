cf44a42bb8f1baff9c1a7146c397db35c48e4b6d
Simon Wheatley <simonw@automattic.com>
2025-10-04 11:15:25 +0100

    Update sync_octopus_tado.py
    Add debug to show URL

--- sync_octopus_tado.py
+++ sync_octopus_tado.py
@@ -13,6 +13,7 @@ def get_meter_reading_total_consumption(api_key, mprn, gas_serial_number):
     """
     period_from = datetime(2000, 1, 1, 0, 0, 0)
     url = f"https://api.octopus.energy/v1/gas-meter-points/{mprn}/meters/{gas_serial_number}/consumption/?group_by=quarter&period_from={period_from.isoformat()}Z"
+    print(f"https://api.octopus.energy/v1/gas-meter-points/{mprn}/meters/{gas_serial_number}/consumption/?group_by=quarter&period_from={period_from.isoformat()}Z")
     total_consumption = 0.0
 
     while url:
